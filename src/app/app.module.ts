386e796507f5447748fddd3b0763a60d769ba743
milesclark0 <clark658@purdue.edu>
2022-06-10 18:35:07 -0400

    Nothing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { Router } from '@angular/router';
 import { EmployeeComponent } from './employee/employee.component';
 import { EmployeeService } from './employee/employee.service';
+import { AuthenticationService } from './TokenAuth/authentication.service';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
@@ -24,7 +26,7 @@ import { CreateAccountComponent } from './create-account/create-account.componen
     ReactiveFormsModule,
     FormsModule
   ],
-  providers: [EmployeeService],
+  providers: [EmployeeService, AuthenticationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
